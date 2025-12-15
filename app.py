a98f80aef2b7498ffa52c7edd7300ab4aaee3f12
ErrorNoSlash <187196647+ErrorNoSlash@users.noreply.github.com>
2025-12-15 16:39:57 +0100

    game now closes without errors

--- app.py
+++ app.py
@@ -7,15 +7,20 @@ pygame.init()
 # =====================================================
 # deze functie zitten alle dingen in die gerund worden.
 # =====================================================
+
+# closes game properly
+def close_game():
+    pygame.quit()
+
 def main():
     surface = create_main_surface()
     clock = pygame.time.Clock()
     font = pygame.font.Font(None, 36)
     start_time = time.time()
-
     health = 100  # Starting health
+    running = True # run game status
 
-    while True:
+    while running:
         surface.fill((0, 0, 0))  # Clear screen with black
 
         # Calculate elapsed time
@@ -31,16 +36,17 @@ def main():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+                running = False
 
             # checks for escape and quits
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     print("closed game")
-                    pygame.quit()
+                    running = False
 
         clock.tick(60)  # 60 FPS
+
+    close_game() # cleanly close game
 
 # no touchy, supposed to run
 main()
