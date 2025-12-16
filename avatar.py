7830b39617f0fc2a7be356ae759b0fef0219395e
Glen Zhang <glen.zhang@student.ucll.be>
2025-12-16 15:36:14 +0100

    Revert "Added avatar rotation according to cursor"
    This reverts commit ddde45fded8d33e0e93944df7d16b618c6381bae.

--- avatar.py
+++ avatar.py
@@ -9,15 +9,6 @@ class Avatar:
         self.rect = pygame.Rect((screen_width - self.width) // 2, screen_height - self.height - 10, self.width, self.height)
         self.speed = 5
 
-        #Load PNG as the base image
-        self.base_image = pygame.image.load("player.png").convert_alpha()
-
-        #Fit to desired size
-        self.base_image = pygame.transform.scale(self.base_image, (self.width, self.height))
-
-        #Current image to be drawn
-        self.image = self.base_image
-
     def update(self, keys, screen_width, screen_height):
         #Move left or right with arrow keys
 
@@ -32,19 +23,7 @@ class Avatar:
             self.rect.y -= self.speed
         if keys[pygame.K_DOWN] and self.rect.bottom < screen_height:
             self.rect.y += self.speed
-
-        #Rotation toward mouse
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        dx = mouse_x - self.rect.centerx
-        dy = mouse_y - self.rect.centery
-        angle = math.degrees(math.atan2(-dy, dx))
-
-        #Rotate the image
-        self.image = pygame.transform.rotate(self.base_image, angle - 90)
-        
-        #Update rect to image size and keep center
-        self.rect = self.image.get_rect(center=self.rect.center)
-
+            
     def get_avatar_position(self):
         #gets avatar position and returns it
         avatar_position = [self.rect.x + (self.width/4), self.rect.y + (self.height/2)]
@@ -52,5 +31,4 @@ class Avatar:
         return avatar_position
     
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
-    
+        pygame.draw.rect(surface, (255, 255, 0), self.rect)
